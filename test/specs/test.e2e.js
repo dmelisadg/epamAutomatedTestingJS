08d952b310588512894bcdd60fc62d48f0a59777
dmelisadg <dmdoming@gmail.com>
2023-10-17 17:41:13 -0500

    Added: Homework 2 - IU test

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -14,8 +14,8 @@ const { describe, it } = require('mocha')
 //             'You logged into a secure area!')
 //     })
 // })
-describe("Test suite", ()=>{
 
+describe("Test suite - Homework 1", ()=>{
     it("First test", async ()=>{
         await browser.url("https://www.python.org/");
         const pageTitle = await browser.getTitle();
@@ -40,3 +40,57 @@ describe("Test suite", ()=>{
     })
 })
 
+describe("Test suite - Homework 2", ()=>{
+
+    it("First test", async ()=>{
+        await browser.url("https://www.wolfram.com/mathematica/");
+        const headerID = await $('#_header');
+        //console.log(await headerID.isExisting())
+        expect(await headerID.isExisting()).toEqual(true);
+    });
+
+    it("Second test", async ()=>{
+        await browser.url("https://www.wolfram.com/mathematica/")
+        const headerID = await $('/html/body/header[2]/div/nav/div[2]/ul/li[2]').click({
+            button: "0",
+            x:25.74,
+            y:13
+        });
+        await 
+        //console.log(await headerID)
+        expect(await headerID.getTitle()).toEqual('Download a Free Trial of Mathematica');
+    });
+
+    it("Third test", async ()=>{
+        await browser.url("https://www.wolfram.com/mathematica/")
+        const iconSearch = await $('/html/body/header[1]/div[2]/nav/div[6]/a/i[2]').click();
+        await browser.pause(1000);
+        const valueSearch = await $('/html/body/header[1]/div[2]/nav/div[6]/ul/li/form/div/input');
+        await valueSearch.setValue('orthogonal')
+        await browser.pause(4000);
+    });
+
+    it("Fourth test", async ()=>{
+        await browser.url("https://www.wolfram.com/mathematica/")
+        const productIcon = await $('/html/body/header[1]/div[2]/nav/div[1]')
+        // console.log(await productIcon);
+        // await browser.pause(4000);
+        expect(await productIcon.isExisting()).toEqual(true);
+    });
+
+    it("Fifth test", async ()=>{
+        await browser.url("https://www.wolfram.com/mathematica/")
+        const navProd = await $('/html/body/header[1]/div[2]/nav')
+        const divProd = await navProd.$$('div')[1].$('a').isClickable()
+       // console.log(await divProd.isClickable());
+        expect(divProd).toEqual(false);
+    });
+
+})
+
+// it("Second test", async ()=>{
+//     await browser.url("https://www.wolfram.com/mathematica/")
+//     const headerID = await $('/html/body/header[1]/div[1]');
+//     //console.log(await headerID)
+//     expect(await headerID.getProperty('id')).toEqual('_header-t');
+// });
