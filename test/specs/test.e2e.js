54f9751e3aa49c594a671bdf7fd4fa6bd37c7646
dmelisadg <dmdoming@gmail.com>
2023-11-10 09:30:23 -0500

    Fixed: Name, CSS and XPath selectors on Homework 1

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -15,82 +15,150 @@ const { describe, it } = require('mocha')
 //     })
 // })
 
-describe("Test suite - Homework 1", ()=>{
-    it("First test", async ()=>{
+describe("Homework 1 - Testing at least 4 scenarios trying to use both XPath and CSS selectors", ()=>{
+    beforeEach(async ()=>{
         await browser.url("https://www.python.org/");
+    })
+    it("First test - WebPage is working right", async ()=>{
         const pageTitle = await browser.getTitle();
         expect(pageTitle).toEqual("Welcome to Python.org")
     });
-
-    it("Second test", async ()=>{
-        const aText = await $("//*[@id='news']/a");
+    it("Second test - ID CSS Selector", async ()=>{
+        const aText = await $("[id='news']");
         expect(await aText.getText()).toEqual('News');
     });
-
-    it("Third test", async ()=>{
-        await $('//*[@id="events"]/a').click()
-        const aLink = await $('//*[@id="content"]/div/aside/div/p[1]/a');
+    it("Third test - ID CSS Selector and XPath", async ()=>{
+        await $('[id="events"]').click()
+        const aLink = await $('/html/body/div/header/div/nav/ul/li[7]/a');
         expect(await aLink.getAttribute('href')).toHaveHref('http://lmorillas.github.io/python_events/');
     });
 
-    it("Fourth test", async ()=>{
-        const element = await $('//*[@id="success-stories"]')
+    it("Fourth test - ID CSS Selector display menu", async ()=>{
+        const element = await $('[id="success-stories"]')
         const dispElement = await element.isDisplayed();
         expect(await dispElement).toEqual(true);
     })
-})
-
-describe("Test suite - Homework 2", ()=>{
-
-    it("First test", async ()=>{
-        await browser.url("https://www.wolfram.com/mathematica/");
-        const headerID = await $('#_header');
-        //console.log(await headerID.isExisting())
-        expect(await headerID.isExisting()).toEqual(true);
+ 
+    it("Fifth test - ID CSS Selector and XPath click button and open link", async ()=>{
+        await $('[id="events"]').click()
+        const aLink = await $('/html/body/div/div[3]/div/aside/div/p[1]/a');
+        expect(await aLink.getAttribute('href')).toHaveHref('http://lmorillas.github.io/python_events/');
     });
+ })
 
-    it("Second test", async ()=>{
-        await browser.url("https://www.wolfram.com/mathematica/")
-        const headerID = await $('/html/body/header[2]/div/nav/div[2]/ul/li[2]').click({
-            button: "0",
-            x:25.74,
-            y:13
-        });
-        await 
-        //console.log(await headerID)
-        expect(await headerID.getTitle()).toEqual('Download a Free Trial of Mathematica');
-    });
+//describe("Test suite - Homework 2", ()=>{
 
-    it("Third test", async ()=>{
-        await browser.url("https://www.wolfram.com/mathematica/")
-        const iconSearch = await $('/html/body/header[1]/div[2]/nav/div[6]/a/i[2]').click();
-        await browser.pause(1000);
-        const valueSearch = await $('/html/body/header[1]/div[2]/nav/div[6]/ul/li/form/div/input');
-        await valueSearch.setValue('orthogonal')
-        await browser.pause(4000);
-    });
-
-    it("Fourth test", async ()=>{
-        await browser.url("https://www.wolfram.com/mathematica/")
-        const productIcon = await $('/html/body/header[1]/div[2]/nav/div[1]')
-        // console.log(await productIcon);
-        // await browser.pause(4000);
-        expect(await productIcon.isExisting()).toEqual(true);
-    });
-
-    it("Fifth test", async ()=>{
-        await browser.url("https://www.wolfram.com/mathematica/")
-        const navProd = await $('/html/body/header[1]/div[2]/nav')
-        const divProd = await navProd.$$('div')[1].$('a').isClickable()
-       // console.log(await divProd.isClickable());
-        expect(divProd).toEqual(false);
-    });
-
-})
+// it("First test", async ()=>{
+//     await browser.url("https://www.wolfram.com/mathematica/");
+//     const headerID = await $('#_header');
+//     //console.log(await headerID.isExisting())
+//     expect(await headerID.isExisting()).toEqual(true);
+// });
 
 // it("Second test", async ()=>{
+//     await browser.url("https://www.wolfram.com/mathematica/")
+//     await $('#_logo').click({
+//         button: 0,
+//         x:100
+//     });
+//     await browser.pause(10000);
+//     const headerID = await $('/html/head')
+//     expect(await headerID.getTitle()).toEqual('Wolfram: La unión entre la computación y el conocimiento');
+// });
+
+// it("Third test", async ()=>{
+//     await browser.url("https://www.wolfram.com/mathematica/")
+//     const iconSearch = await $('/html/body/header[1]/div[2]/nav/div[6]/a/i[2]').click();
+//     await browser.pause(1000);
+//     const valueSearch = await $('/html/body/header[1]/div[2]/nav/div[6]/ul/li/form/div/input');
+//     await valueSearch.setValue('orthogonal')
+//     await browser.pause(4000);
+// });
+
+// it("Fourth test", async ()=>{
+//     await browser.url("https://www.wolfram.com/mathematica/")
+//     const productIcon = await $('/html/body/header[1]/div[2]/nav/div[1]')
+//     // console.log(await productIcon);
+//     // await browser.pause(4000);
+//     expect(await productIcon.isExisting()).toEqual(true);
+// });
+
+// it("Fifth test", async ()=>{
+//     await browser.url("https://www.wolfram.com/mathematica/")
+//     const navProd = await $('/html/body/header[1]/div[2]/nav')
+//     const divProd = await navProd.$$('div')[1].$('a').isClickable()
+//    // console.log(await divProd.isClickable());
+//     expect(divProd).toEqual(false);
+// });
+
+
+// it("Sixth test", async ()=>{
 //     await browser.url("https://www.wolfram.com/mathematica/")
 //     const headerID = await $('/html/body/header[1]/div[1]');
 //     //console.log(await headerID)
 //     expect(await headerID.getProperty('id')).toEqual('_header-t');
 // });
+//})
+
+// describe("Test suite - Homework 3", () => {
+//     // it("First test", async () => {
+//     //     await browser.url('https://www.kalley.com.co');
+//     //     const input = await $('#js-site-search-input')
+//     //     await browser.execute(
+//     //         function (input) {
+//     //             return input.value = "Computadoras"
+//     //         }, input)
+//     //     //await $('button#js-search-button').click();
+//     //     browser.submitForm('form[name="search_form_SearchBox"]')
+//     //     await browser.pause(10000)
+//     // });
+
+//     // it("Second Test - wait until", async () => {
+//     //     await browser.url('https://www.youtube.com/');
+//     //     await $("#guide-button").click();
+//     //     await browser.waitUntil(
+//     //         async () => await $(".style-scope.tp-yt-app-drawer.visible").isDisplayed(),
+//     //         {timeout: 5000, interval: 600, timeoutMsg: "not loaded"}
+//     //     );
+//     // });
+
+//     // it("Third Test ", async ()=>{
+//     //     await browser.url('https://mousetester.com/');
+//     //     const clicks = await $('#clickMe');
+//     //     await clicks.doubleClick();
+//     //     await browser.pause(5000)
+//     // });
+
+//     // it('Fourth Test', async () => {
+//     //     await browser.url('https://www.e-education.psu.edu/geog777/h5p/embed/1950');
+//     //     const elem = await $('.h5p-hidden-read');
+//     //     const target = await $('.h5p-inner.ui-droppable');
+//     //     await browser.pause(3000)
+//     //     await elem.dragAndDrop(target);
+//     //     await browser.pause(10000)
+//     // })
+
+//     // it('Fifth Test', async () => {
+//     //     await browser.url('https://www.fisicalab.com/tema/optica-geometrica');
+//     //     await browser.setCookies({
+//     //         name: 'test1',
+//     //         value: 'Galletita Uno!'
+//     //     })
+//     //     const cookies = await browser.getCookies('test1')
+//     //     console.log(cookies);
+//     // });
+
+//     // it('Fifth Test', async () => {
+//     //     const key = "localStorageKey";
+//     //     const value = "localStorageValue";
+        
+//     //     await browser.execute(function (key, value) {
+//     //         window.localStorage.setItem(key, value);
+//     //     }, key, value);
+//     //     const readValue = await browser.execute(function (key) {
+//     //         return window.localStorage.getItem(key);
+//     //     }, key);
+//     //     console.log(readValue);
+//     //     expect (readValue).toEqual('localStorageValue')
+//     // });
+// })
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,8 @@
+const {
+    existsSync,
+    mkdirSync
+} = require("fs");
+const allure = require("./node_modules/allure-commandline")
 exports.config = {
     //
     // ====================
@@ -52,7 +57,11 @@ exports.config = {
     //
     capabilities: [{
         browserName: 'chrome'
-    }],
+    },
+    // {
+    //     browserName: 'firefox'
+    // }
+],
 
     //
     // ===================
@@ -123,9 +132,23 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [
+        'spec', 
+        ["junit",{
+        outputDir:"./report",
+        outputFileFormat: function (options) {
+            return  `result-${options.cid}.xml`;
+        },
+    }],
+    ["allure", {
+        outputDir: "./allure-results",
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+        }
+      ]
+],
 
-    
+
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
@@ -227,8 +250,19 @@ exports.config = {
      * @param {boolean} result.passed    true if test has passed, otherwise false
      * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
      */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
+    afterTest: async function (test, context, { error, result, duration, passed, retries }) {
+        if (error) {
+            console.log(`Screenshot for the failed test ${test.title} is saved`);
+            const filename = test.title + '.png';
+            const dirPath = './screenshots/';
+            if (!existsSync(dirPath)) {
+                mkdirSync(dirPath, {
+                    recursive: true,
+                });
+            }
+            await browser.saveScreenshot(dirPath + filename);
+        }
+    },
 
 
     /**
@@ -271,8 +305,21 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {<Object>} results object containing test results
      */
-    // onComplete: function(exitCode, config, capabilities, results) {
-    // },
+    onComplete: function () {
+        const reportError = new Error('Could not generate Allure report');  
+        const generation = allure(['generate', 'allure-results', '--clean']);
+        return new Promise((resolve, reject) => {
+          const generationTimeout = setTimeout(() => reject(reportError), 5000);
+          generation.on('exit', function (exitCode) {
+            clearTimeout(generationTimeout);
+            if (exitCode !== 0) {
+              return reject(reportError);
+            }
+            console.log('Allure report successfully generated');
+            resolve();
+          });
+        });
+      },
     /**
     * Gets executed when a refresh happens.
     * @param {string} oldSessionId session ID of the old session
